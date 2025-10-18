4d1bfc50038265cffb4754d21e72752df2fe0d5b
MeetToast5578 <alizadeyunus@gmail.com>
2025-10-18 19:25:54 +0400

    palindrome 2

---
+++ Palindrome_2.py
@@ -0,0 +1,6 @@
+word_list = ["kayak","deified","rotator","Salam", "apple", "banana", "rotator"]
+longest_pal_word = ""
+for word in word_list:
+    if word == word[::-1] and word > longest_pal_word:
+        longest_pal_word = word
+print(longest_pal_word)
