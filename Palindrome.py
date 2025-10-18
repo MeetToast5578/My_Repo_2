30c51e22ffa2bc3b5475d94c2611705d44ab7bb4
MeetToast5578 <alizadeyunus@gmail.com>
2025-10-18 19:18:33 +0400

    commit

---
+++ Palindrome.py
@@ -0,0 +1,11 @@
+word_list = ["kayak","deified","rotator","Salam", "apple", "banana"]
+palindrome_words = []
+longest_pal_word = ""
+for word in word_list:
+    if word == word[::-1]:
+        palindrome_words.append(word)
+for i in palindrome_words:
+    if i > longest_pal_word:
+        longest_pal_word = i
+print(f"Palindrome words: {palindrome_words}")
+print(f"Longest palindrome word: {longest_pal_word}")
