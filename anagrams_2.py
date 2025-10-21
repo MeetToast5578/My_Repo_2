265f2121e6a7ed20d5093a819b3b7ca3a85e65b7
MeetToast5578 <alizadeyunus@gmail.com>
2025-10-21 19:02:44 +0400

    anagrams_2_1

--- anagrams_2.py
+++ anagrams_2.py
@@ -2,10 +2,14 @@ sentence = "Listen to the silent notes"
 
 word_list = sentence.split()
 sorted_word_list = []
+count = 0
 for i in word_list:
     sorted_word = ''.join(sorted(i.lower()))
-    print(sorted_word)
     sorted_word_list.append(sorted_word)
 for q in range(len(sorted_word_list)):
     if sorted_word_list.count(sorted_word_list[q]) > 1:
+        count+=1
         print(word_list[q])
+if count == 0:
+    print("-1")
+    
